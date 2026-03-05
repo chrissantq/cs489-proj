86f90434a6c24c0e392ed020fde5055d768f8af6
chrissantq <crquinney1@gmail.com>
2026-03-05 14:31:43 -0500

    added microphone toggle functionality, just limited to claps right now

--- cs489-proj.ino
+++ cs489-proj.ino
@@ -1,5 +1,4 @@
-// analog pins
-#define MICPIN A0
+#include <cstdlib>
 
 // digital pins
 #define TOGGLEPIN 2 // right now button to toggle relay, eventually
@@ -9,31 +8,71 @@
 #define LATCHPIN 12
 #define AUDIOPIN 9
 
-// devtab and devid constants
+// devtab constants
 #define NDEVS 10     // max number of unique types of devices
 #define NDEV_INST 5  // max number of each device type
-#define RELAY 0
+#define NRELAYS 4    // number of relays
+
+// device ids
+#define RELAYID 0
+#define MICID 1
 
 // other constants
 #define DEBOUNCE 200 // ignore button inputs within 200ms of last
 
+// device structs
+typedef struct relay_dev {
+  int devnum;     // specific dev num
+  int mask;       // bitmask to toggle state via 74hc595 chip
+  bool state;     // true = on, false = off
+} relay_t;
+
+typedef struct microphone_dev {
+  int devnum;     // specific device number
+  int threshold;  // sound difference to register
+  int pin;        // pin device using
+  int reading;    // microphone analogread
+} mic_t;
+
 /*
 *   dev table
-*   for now just store the one thing used by dev, later
-*   change to store pointers to struct for specific dev
 *
 *   rows: device; cols: dev num
 *   0 -> relays
+*   1 -> microphone
 */
-int devtab[NDEVS][NDEV_INST] = {{0}};
+void * devtab[NDEVS][NDEV_INST] = {0};
 
 byte chipstate = 0b00000000; // current state of the 74hc595 chip
+int noise_avg = 512;
 volatile int devnum = 0; // not volatile rn, prob will be later
 volatile bool toggled = false;
 volatile unsigned long lastPressTime = 0;
 
+// device setup function
+void devinit() {
+
+  // setup relay devices
+  for (int i = 0; i < NRELAYS; i++) {
+    relay_t * newRelay = (relay_t*)malloc(sizeof(relay_t));
+    newRelay->devnum = i;
+    newRelay->mask = 1 << (i+1);
+    newRelay->state = false;
+    devtab[RELAYID][newRelay->devnum] = newRelay;
+  }
+
+  // set up microphone device
+  mic_t * microphone = (mic_t*)malloc(sizeof(mic_t));
+  microphone->devnum = 0;
+  microphone->threshold = 300;
+  microphone->pin = A5;
+  microphone->reading = 0;
+  devtab[MICID][microphone->devnum] = microphone;
+
+}
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(DATAPIN, OUTPUT);
   pinMode(CLOCKPIN, OUTPUT);
   pinMode(LATCHPIN, OUTPUT);
@@ -42,12 +81,8 @@ void setup() {
   // interrupt when button is pressed
   attachInterrupt(digitalPinToInterrupt(TOGGLEPIN), toggleISR, FALLING);
 
-  // setup device table
-  // set relay to respective bit value to toggle
-  // 74hc595 chip output (00000010 through 00010000)
-  for (int i = 0; i < 4; i++) {
-    devtab[RELAY][i] = 1 << (i+1);
-  }
+  // init devices
+  devinit();
 
 }
 
@@ -61,14 +96,14 @@ void toggleISR() {
 }
 
 // update the state
-void updateRelay(int devnum) {
-  chipstate ^= devtab[RELAY][devnum]; // toggle relay
+void updateRelay(relay_t* relay) {
+  chipstate ^= relay->mask; // toggle relay
   // push out new state
   digitalWrite(LATCHPIN, LOW);
   shiftOut(DATAPIN, CLOCKPIN, MSBFIRST, chipstate);
   digitalWrite(LATCHPIN, HIGH);
   // play little tone on speaker
-  tone(AUDIOPIN, 100*devnum, 150);
+  tone(AUDIOPIN, 100*(relay->devnum), 150);
   delay(150);
   tone(AUDIOPIN, 200, 50);
   delay(50);
@@ -77,8 +112,25 @@ void updateRelay(int devnum) {
 void loop() {
   if (toggled) {
     toggled = false;
-    updateRelay(devnum);
+    relay_t *relay = (relay_t*)devtab[RELAYID][devnum];
+    updateRelay(relay);
     devnum = (devnum + 1) % 4; // for now just cycle between the relays
                              // later, will be chosen based off command
   }
+
+  // poll mic for loud noise (ie clap) to toggle
+  // prob switch to interrupts using LM393 chip to be more
+  // reliable, this works decently well tho for now
+  mic_t * mic = (mic_t*)devtab[MICID][0];
+  mic->reading = analogRead(mic->pin);
+  noise_avg = (noise_avg * 15 + mic->reading) / 16;
+  if (abs(mic->reading - noise_avg) > mic->threshold) {
+    delay(10); // detect short burst of sound (ie clap)
+    mic->reading = analogRead(mic->pin);
+    if (abs(mic->reading - noise_avg) < (mic->threshold / 2)) {
+      toggled = true;
+    }
+  }
+
+
 }
